f035ec6490b774d6b9fbf535e6d389693823ee19
Xoldarov Temur <69597351+xaldarof@users.noreply.github.com>
2023-03-30 09:48:35 +0500

    Update aes_encryption.dart

--- lib/src/encryption/aes_encryption.dart
+++ lib/src/encryption/aes_encryption.dart
@@ -10,9 +10,9 @@ class AesEncryptionImpl implements AesEncryption {
   @override
   String decrypt(Encrypted encryptedData, {AESMode? mode}) {
     assert(_encryptionKey.length >= 16);
-    final cipherKey = Key.fromUtf8(_encryptionKey.substring(0, 16));
+    final cipherKey = Key.fromUtf8(_encryptionKey);
     final encryptService = Encrypter(AES(cipherKey, mode: mode ?? AESMode.cbc));
-    final initVector = IV.fromUtf8(_encryptionKey.substring(0, 16));
+    final initVector = IV.fromUtf8(_encryptionKey);
 
     return encryptService.decrypt(encryptedData, iv: initVector);
   }
@@ -20,9 +20,9 @@ class AesEncryptionImpl implements AesEncryption {
   @override
   Encrypted encrypt(String plainText, {AESMode? mode}) {
     assert(_encryptionKey.length >= 16);
-    final cipherKey = Key.fromUtf8(_encryptionKey.substring(0, 16));
+    final cipherKey = Key.fromUtf8(_encryptionKey);
     final encryptService = Encrypter(AES(cipherKey, mode: mode ?? AESMode.cbc));
-    final initVector = IV.fromUtf8(_encryptionKey.substring(0, 16));
+    final initVector = IV.fromUtf8(_encryptionKey);
 
     Encrypted encryptedData = encryptService.encrypt(plainText, iv: initVector);
     return encryptedData;
